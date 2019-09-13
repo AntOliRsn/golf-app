acd2453abb0cf6c84897f2245bf9e7858b642e62
AROSIN <antoine.rosin@sia-partners.com>
2019-09-13 09:25:03 +0200

    add sqlalchimy+marshmallow+flask dependencies

--- backend/backend/extensions.py
+++ backend/backend/extensions.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 cors = CORS()
 db = SQLAlchemy()
+ma = Marshmallow()
